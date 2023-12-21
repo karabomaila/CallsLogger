d8ef279cd30f75ca6719d48e253829f9cffac485
Karabo_Maila <karabo.maila@wits.ac.za>
2023-12-21 15:42:31 +0200

    updates

---
+++ src/test/java/org/logger/callslogger/controller/HomeController.java
@@ -0,0 +1,11 @@
+package org.logger.callslogger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+
+}
