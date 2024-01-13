97e6ab0f32721bfa09d9033c1d158b593551aaea
Karabo_Maila <karabo.maila@wits.ac.za>
2024-01-13 17:44:20 +0200

    - updates

--- src/main/java/org/logger/callslogger/controller/CallsController.java
+++ src/main/java/org/logger/callslogger/controller/CallsController.java
@@ -2,12 +2,18 @@ package org.logger.callslogger.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CallsController {
 
     @GetMapping("logcalls")
     public String logCall(){
+        return "logcallForm";
+    }
+
+    @PostMapping("logcalls")
+    public String addLogCall(){
         return "logcallForm";
     }
 }
